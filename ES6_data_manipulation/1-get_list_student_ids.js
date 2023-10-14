ac97e1bb47911cdfd4cede884d57fca0a2fbdb3a
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-10-14 13:14:44 -0400

    task 1

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+/* enlist-disable */
+function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+        return [];
+    }
+
+    return students.map((student) => student.id);
+}
+
+export default getListStudentIds;
