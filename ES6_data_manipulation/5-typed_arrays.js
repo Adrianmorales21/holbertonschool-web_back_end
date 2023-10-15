ecbdb09ff378d328b9a554316863bc9b5b533a30
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-10-14 23:51:29 -0400

    Task fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,14 +1,9 @@
-/* enlist-disable */
-function createInt8TypedArray(length, position, value) {
-    if (position < 0 || position >= length) {
-        throw new Error('Position outside range');
-    }
+/* eslint-disable */
 
+export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
-    const int8Array = new Int8Array(buffer);
-    int8Array[position] = value;
-
-    return new DataView(buffer);
+    const view = new DataView(buffer, 0);
+    if (position > length - 1) throw Error('Position outside range');
+    view.setInt8(position, value);
+    return view;
 }
-
-export default createInt8TypedArray;
