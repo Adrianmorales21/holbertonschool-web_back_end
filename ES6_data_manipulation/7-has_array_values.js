bfada315fb3224ef438cecfe15e0148d7febeef6
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-10-14 23:54:26 -0400

    fix

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,10 @@
 /* eslint-disable */
 
-export default function hasValuesFromArray(set, array) {
-    const has = array.every((value) => set.has(value));
-    return has;
+export default function cleanSet(set, startString) {
+    let result = '';
+    if (!startString || !startString.length) return result;
+    set.forEach((i) => {
+        if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+    });
+    return result.slice(0, result.length - 1);
 }
