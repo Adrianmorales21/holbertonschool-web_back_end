10ce3b86efe838e24cd6682def458a53863321db
Adrianmorales21 <adrianmorales212003@gmail.com>
2023-10-14 23:55:01 -0400

    fix

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,19 +1,10 @@
-/* enlist-disable */
-function cleanSet(set, startString) {
-    if (typeof startString !== 'string') {
-        return '';
-    }
+/* eslint-disable */
 
-    const filteredValues = new Set();
-
-    for (const value of set) {
-        if (typeof value === 'string' && value.startsWith(startString)) {
-            const restOfValue = value.slice(startString.length);
-            filteredValues.add(restOfValue);
-        }
-    }
-
-    return Array.from(filteredValues).join('-');
+export default function cleanSet(set, startString) {
+    let result = '';
+    if (!startString || !startString.length) return result;
+    set.forEach((i) => {
+        if (i && i.startsWith(startString)) result += `${i.slice(startString.length)}-`;
+    });
+    return result.slice(0, result.length - 1);
 }
-
-export default cleanSet;
